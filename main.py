1ad5156a0ecb27d81217c37d0379d0e02eeb23b9
[mxclover] <mingx91.py@gmail.com>
2017-07-04 21:17:56 +0800

    update main.py

--- main.py
+++ main.py
@@ -12,7 +12,7 @@ from flask import Flask, render_template, request
 app = Flask(__name__)
 
 @app.route('/', methods=['GET','POST'])
-def index():
+def home():
 	weather = ''
 	if request.method == 'POST':
 		if 'query' in request.form.keys():
@@ -20,11 +20,11 @@ def index():
 			weather = get_weather(city_name)
 		elif 'history' in request.form.keys(): 
 			history_info = get_history()
-			return render_template('index.html',info=history_info)
+			return render_template('home.html',info=history_info)
 		elif 'help' in request.form.keys():
 			help_info = get_help()
-			return render_template('index.html',info=help_info)
-	return render_template('index.html',info=weather)
+			return render_template('home.html',info=help_info)
+	return render_template('home.html',info=weather)
 
 history_list = []
 
